33da5b229d27ed7523380f7953e6ef3fe8123f20
JayBadass <jaybadass@naver.com>
2023-08-02 13:36:38 +0900

    feat: two button 구현

---
+++ memo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by Jongbum Lee on 2023/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var todoListButton: UIButton!
+    @IBOutlet weak var doneListButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
